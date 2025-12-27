2648d5fc556961fe21a3578ac9a76d45d63088c0
Momo <github2@5208866.xyz>
2025-12-27 17:00:27 +0200

    Netlify env vars

--- app/server/db/dynamo.ts
+++ app/server/db/dynamo.ts
@@ -1,8 +1,16 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({
-  region: process.env.AWS_REGION ?? "us-east-1", // match your table region
+// const client = new DynamoDBClient({
+//   region: process.env.AWS_DYNAMO_REGION ?? "us-east-1", // match your table region
+// });
+
+export const client = new DynamoDBClient({
+  region: process.env.MY_DYNAMO_REGION!,
+  credentials: {
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY!,
+  },
 });
 
 export const ddb = DynamoDBDocumentClient.from(client);
